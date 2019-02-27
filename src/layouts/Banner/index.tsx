ca964075bfa99d7495251aff5a5490c4c93457d5
abu <1611499758@qq.com>
2019-02-27 22:01:20 +0800

    fix(add banner): 修复了字体大小bug

--- src/layouts/Banner/index.tsx
+++ src/layouts/Banner/index.tsx
@@ -2,11 +2,37 @@ import * as React from 'react'
 import './index.less'
 
 import Config from '@config/index'
+/**
+ * 转换为当前尺寸
+ * @param px 输入px转换
+ */
+function bannerLess(px: number): number {
+    return document.body.clientWidth / 1440 * px
+}
+interface IStyle {
+    [index: string]: string
+}
+const styleBaner: IStyle = {
+    'height': bannerLess(750) + 'px'
+}
+const styleH2: IStyle = {
+    'marginTop': bannerLess(242) + 'px',
+    'fontSize': bannerLess(107) + 'px',
+}
+const styleH3: IStyle = {
+    'marginTop': bannerLess(87) + 'px',
+    'fontSize': bannerLess(41) + 'px',
+}
+const styleH4: IStyle = {
+    'marginTop': bannerLess(43) + 'px',
+    'fontSize': bannerLess(41) + 'px',
+}
+
 const Banner = () => (
-    <div className='banner' >
-        <h2>{Config.banner.bannerTitle}</h2>
-        <h3>时间：{Config.banner.bannerDate}</h3>
-        <h3>地点：{Config.banner.bannerPlace}</h3>
+    <div className='banner' style={styleBaner} >
+        <h2 style={styleH2} >{Config.banner.bannerTitle}</h2>
+        <h3 style={styleH3} >时间：{Config.banner.bannerDate}</h3>
+        <h3 style={styleH4} >地点：{Config.banner.bannerPlace}</h3>
     </div>
 )
 export default Banner
