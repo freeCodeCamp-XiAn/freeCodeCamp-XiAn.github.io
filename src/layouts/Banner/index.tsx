6a51c5acfa804f00bffc8188bd122ec4d0bffccd
guzhongren <guzhongren@live.cn>
2019-03-02 17:31:53 +0800

    container(page): 优化页面

--- src/layouts/Banner/index.tsx
+++ src/layouts/Banner/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import './index.less'
 
-import Config from '@config/index'
+// import Config from '@config/index'
 
 const Banner = () => (
     <div className='banner'>
-		<div>
+		{/* <div>
 			<span className='title'>{Config.banner.title}</span>
 			<span className='date'>时间：{Config.banner.date}</span>
 			<span className='address'>地点：{Config.banner.address}</span>
-		</div>
+		</div> */}
     </div>
 )
 export default Banner
