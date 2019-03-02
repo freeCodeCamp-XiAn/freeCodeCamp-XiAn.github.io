5f22f56a1a246d39b6f3437a2f2d3f39d2f6af7a
guzhongren <guzhongren@live.cn>
2019-03-02 16:58:46 +0800

    Revert "去掉了banner文字"

--- src/layouts/Banner/index.tsx
+++ src/layouts/Banner/index.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react'
 import './index.less'
-/**
- * 转换为当前尺寸
- * @param px 输入px转换
- */
-function bannerLess(px: number): number {
-    return document.body.clientWidth / 1440 * px
-}
-interface IStyle {
-    [index: string]: string
-}
-const styleBaner: IStyle = {
-    'height': bannerLess(750) + 'px'
-}
+
+import Config from '@config/index'
 
 const Banner = () => (
-    <div className='banner' style={styleBaner} >
-        <br/>
+    <div className='banner'>
+		<div>
+			<span className='title'>{Config.banner.title}</span>
+			<span className='date'>时间：{Config.banner.date}</span>
+			<span className='address'>地点：{Config.banner.address}</span>
+		</div>
     </div>
 )
 export default Banner
