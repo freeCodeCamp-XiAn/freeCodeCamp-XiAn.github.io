64574ff1f2728445a2c6d48b26cf0932ffcda9f8
abu <1611499758@qq.com>
2019-03-09 13:05:22 +0800

    fix(bottom): banner组件类型修复

--- src/components/Bottom/index.tsx
+++ src/components/Bottom/index.tsx
@@ -3,53 +3,74 @@ import Config from '@config/index'
 import './index.less'
 import Image from '@components/RoundImage'
 
+interface Iimg {
+    name: string
+    url: string
+    active: number
+}
+
+type IFun = (v: any, i: number) => any
+
+interface IData {
+    [index: number]: Iimg
+    length: number
+    map: (fun: IFun) => any
+}
+interface IPartner {
+    name: string
+    data: IData
+}
+interface IVolunteer {
+    name: string
+    data: IData
+}
 interface IState {
-	partner: object
-	volunteer: object
+    partner: IPartner
+    volunteer: IVolunteer
 }
 
 export default class Bottom extends React.Component<any, IState> {
-	constructor(props) {
-		super(props)
-		this.state = {
-			partner: Config.partner,
-			volunteer: Config.volunteer
-		}
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            partner: Config.partner,
+            volunteer: Config.volunteer
+        }
+    }
 
-	getImage(conf: any[], type: string) {
-		return (
-			conf.length &&
-			conf.map((v: any, i: number) => {
-				return (
-					<Image
-						key={i}
-						name={v.name}
-						url={v.url}
-						active={v.active}
-						type={type}
-					/>
-				)
-			})
-		)
-	}
+    getImage(conf: IData, type: string) {
+        return (
+            conf.length &&
+            conf.map((v: any, i: number) => {
+                return (
+                    <Image
+                        key={i}
+                        name={v.name}
+                        url={v.url}
+                        active={v.active}
+                        type={type}
+                    />
+                )
+            })
+        )
+    }
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="partner">
-                    <div className="title">合作伙伴</div>
-                    <div className="details">
-                        {this.getImage(this.state.partner['data'], this.state.partner['name'])}
-                    </div>
+    render() {
+        return (
+            <React.Fragment>
+                <div className='partner'>
+				<div className='title'>合作伙伴</div>
+				<div className='details'>
+				{this.getImage(this.state.partner.data, this.state.partner.name)}
 				</div>
-				<div className="volunteer">
-                    <div className="title">志愿者</div>
-                    <div className="details">
-					    {this.getImage(this.state.volunteer['data'], this.state.volunteer['name'])}
-                    </div>
-                </div>
-			</React.Fragment>
-		)
-	}
+                </div>
+                <div className='volunteer'>
+					<div className='title'>志愿者</div>
+					<div className='details'>
+	                     {this.getImage(this.state.volunteer.data, this.state.volunteer.name)}
+					</div>
+				</div>
+            </React.Fragment>
+        )
+    }
 }
